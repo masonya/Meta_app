2c2b13711dd550b1b9714973595ee70b5535c92b
masonya <masonya@yandex.ru>
2019-03-22 11:32:03 +0300

    Work snapshot

--- app/models/account_transfer.rb
+++ app/models/account_transfer.rb
@@ -2,7 +2,7 @@ class AccountTransfer < ApplicationRecord
   belongs_to :transmitter, class_name: "User", foreign_key: "transmitter_id"
   belongs_to :inheritor, class_name: "User", foreign_key: "inheritor_id", optional: true
 
-  belongs_to :social_account, foreign_key: "transferable_id", dependent: :destroy
+  # belongs_to :social_account, foreign_key: "transferable_id", dependent: :destroy
   #belongs_to :document, foreign_key: "transferable_id", dependent: :destroy
 
   belongs_to :transferable, polymorphic: true
