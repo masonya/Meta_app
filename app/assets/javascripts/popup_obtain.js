d12d6411935e36415480a48e3236732658e276b1
bitterglaze <onetanya.14@yandex.ru>
2019-05-06 21:20:35 +0300

    новый попап в получаю
    все почти ок, не понимаю че написать, чтобы карточки по ховеру не все раздвигались, а только нужная

---
+++ app/assets/javascripts/popup_obtain.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.editing').hover(
+    function() {
+      $('.card').css('height','279px');
+      }
+  );
+
+  // $("div.user").click(function() {
+  //    $("div#up").removeClass();
+  //    $("div#up").addClass("user_popup_view");
+  //   });
+
+});
