594ed294dee9bf2984b479131bb22949cb54b766
bitterglaze <onetanya.14@yandex.ru>
2019-05-18 20:34:54 +0300

    подтянула стили чуть

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_131616) do
+ActiveRecord::Schema.define(version: 2019_05_17_070855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
